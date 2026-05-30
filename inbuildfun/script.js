e62dff86303a04f304e1c6cee2e80d70215cca9d
CTxPAIN <PRINCEOFFICIALBLOG@GMAIL.COM>
2026-05-30 23:01:54 +0530

    foreach,forin,forof,iife

--- inbuildfun/script.js
+++ inbuildfun/script.js
@@ -23,41 +23,47 @@
 
 //math fun
 
-let arr=[34,2,2,1,3,42,2];
-console.log(Math.round(2.8));
-console.log(Math.floor(3.5));
-console.log(Math.floor(35/2));
-console.log(Math.max(9,4,3,3,2));
-console.log(Math.min(...arr));
-console.log(Math.abs(4-9));
-
-let name="john";
-
-console.log(`hello,${name}`);
+// let arr=[34,2,2,1,3,42,2];
+// console.log(Math.round(2.8));
+// console.log(Math.floor(3.5));
+// console.log(Math.floor(35/2));
+// console.log(Math.max(9,4,3,3,2));
+// console.log(Math.min(...arr));
+// console.log(Math.abs(4-9));
 
 
-//replace 
-console.log(name.replace("j","h"));
+//template literals
+// let name="john";
 
-//includes
-console.log(name.includes("jo"));
+// console.log(`hello,${name}`);
+
+
+// //replace 
+// console.log(name.replace("j","h"));
+
+// //includes
+// console.log(name.includes("jo"));
 
 
 //switch
-let day="tuesday";
-switch (day){
-    case "monday":
-        console.log("today is school");
-        break;
+// let day="tuesday";
+// switch (day){
+//     case "monday":
+//         console.log("today is school");
+//         break;
     
-    case "tuesday":
-        console.log("tomorrow is not school");
-        break;
+//     case "tuesday":
+//         console.log("tomorrow is not school");
+//         break;
     
-    default:
-        console.log("normal day")
-}
+//     default:
+//         console.log("normal day")
+// }
 
 
+//trim 
+let str="   hello world   ";
+console.log(str);
+console.log(str.trim());
 
 
