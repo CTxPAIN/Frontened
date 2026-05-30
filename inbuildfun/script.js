ba22e109e3bb53c02388c4f39d7a724b62590d12
CTxPAIN <PRINCEOFFICIALBLOG@GMAIL.COM>
2026-05-30 19:40:38 +0530

    initial commit

---
+++ inbuildfun/script.js
@@ -0,0 +1,63 @@
+// //1.slice
+
+// let arr=[2,4,5,14,2];
+// let ans=arr.slice(0,4);
+// console.log(ans);
+
+// ans=arr.slice(-2);
+// console.log(ans);
+
+
+// arr=["hello","lenovo","macbook","apple","samsung"];
+// ans=arr.slice(1,3);
+// console.log(ans);
+
+
+// // copy a full arry using a slice
+
+// let num=[4,5,6,3,2];
+// let newNum=[];
+// newNum.push(num.slice(0,num.length));
+// console.log(newNum);
+
+
+//math fun
+
+let arr=[34,2,2,1,3,42,2];
+console.log(Math.round(2.8));
+console.log(Math.floor(3.5));
+console.log(Math.floor(35/2));
+console.log(Math.max(9,4,3,3,2));
+console.log(Math.min(...arr));
+console.log(Math.abs(4-9));
+
+let name="john";
+
+console.log(`hello,${name}`);
+
+
+//replace 
+console.log(name.replace("j","h"));
+
+//includes
+console.log(name.includes("jo"));
+
+
+//switch
+let day="tuesday";
+switch (day){
+    case "monday":
+        console.log("today is school");
+        break;
+    
+    case "tuesday":
+        console.log("tomorrow is not school");
+        break;
+    
+    default:
+        console.log("normal day")
+}
+
+
+
+
